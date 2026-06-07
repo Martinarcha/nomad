e46c6951754a1d25c7cb895910db00bf00a7e32a
martinarcha <archambaulf15@gmail.com>
2026-06-07 16:01:04 -0300

    Build experiences modal layout

--- js/experiences.js
+++ js/experiences.js
@@ -1,7 +1,108 @@
+
+const showcase = document.querySelectorAll(".category-showcase");
+
 fetch("./data/experiences.json")
     .then(response => response.json())
     .then(experiences => {
-        console.log(experiences)
-    })
 
-console.log(experiences);
+        function renderCategory(categoriaActual, contenedor) {
+            const categoryExperiences = experiences.filter(experience => {
+                return experience.category === categoriaActual;
+            });
+
+            const featuredContainer =
+                contenedor.querySelector(".featured-experience");
+
+            const secondaryContainer =
+                contenedor.querySelector(".secondary-experiences");
+
+            featuredContainer.innerHTML = "";
+            secondaryContainer.innerHTML = "";
+
+
+            categoryExperiences.forEach((experience, index) => {
+                if (index === 0) {
+
+                    featuredContainer.innerHTML += `
+
+                    <div class="featured-experience-card" data-id="${experience.id}">
+                        <div class="featured-experience__image">
+                            <img src="${experience.image}"
+                                alt="${experience.alt}">
+                        </div>
+
+                        <div class="featured-experience__content">
+                            <p class="featured-experience__location">
+                                ${experience.location}
+                            </p>
+
+                            <h3 class="featured-experience__title">
+                                ${experience.title}
+                            </h3>
+                        </div>
+
+                    </div>
+
+                `;
+                } else {
+                    secondaryContainer.innerHTML += `
+                    <div class="secondary-experience" data-id="${experience.id}">
+
+                        <div class="secondary-experience__image">
+                            <img src="${experience.image}"
+                                alt="${experience.alt}">
+                        </div>
+
+                        <div class="secondary-experience__content">
+
+                            <p class="secondary-experience__location">
+                                ${experience.location}
+                            </p>
+
+                            <h4 class="secondary-experience__title">
+                                ${experience.title}
+                            </h4>
+
+                        </div>
+
+                    </div>
+                `
+                }
+            });
+
+        }
+        renderCategory("mountains", showcase[0]);
+        renderCategory("ocean", showcase[1]);
+        renderCategory("desert", showcase[2]);
+        renderCategory("forest", showcase[3]);
+
+
+        const cards = document.querySelectorAll(
+            ".featured-experience-card, .secondary-experience"
+        );
+
+        cards.forEach(card => {
+            card.addEventListener("click", () => {
+                const clickedId = card.dataset.id;
+                const actualExperience = experiences.find(experience => {
+                    return experience.id === Number(clickedId);
+                });
+
+                console.log(actualExperience);
+
+            });
+        });
+
+        const modal = document.querySelector(".experience-modal");
+        const modalContent = document.querySelector(".experience-modal__content");
+
+    }
+
+    );
+
+
+
+
+
+
+
