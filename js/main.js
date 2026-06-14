9b7f219c245072e13852b27cd2e80c3d749d7d73
martinarcha <archambaulf15@gmail.com>
2026-06-14 13:26:17 -0300

    feat: add responsive navigation menu interactions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log("NOMAD Connected");
+
+const menuButton = document.querySelector(".menu-button");
+const navMenu = document.querySelector(".nav-menu");
+const header = document.querySelector("header")
+
+menuButton.addEventListener("click", () => {
+    navMenu.classList.toggle("nav-menu-hidden");
+});
+
+document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+        navMenu.classList.add("nav-menu-hidden");
+    }
+});
+
+document.addEventListener("click", (event) => {
+    if (!header.contains(event.target)) {
+        navMenu.classList.add("nav-menu-hidden");
+    }
+});
